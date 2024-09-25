07d858cbd591a051aba65bb562695d1f061128e5
= <gerosullivan@gmail.com>
2024-09-25 20:19:57 +0100

    remove hover to fix mobile double press

--- components/sidebar/items/chat/quick-quiz.tsx
+++ components/sidebar/items/chat/quick-quiz.tsx
@@ -30,8 +30,8 @@ export const QuickQuiz = () => {
     <div
       ref={itemRef}
       className={cn(
-        "hover:bg-accent focus:bg-accent group flex w-full cursor-pointer items-center rounded p-2 hover:opacity-50 focus:outline-none",
-        isActive && "bg-accent"
+        "group flex w-full cursor-pointer items-center rounded p-2 focus:outline-none",
+        isActive ? "bg-accent" : "hover:bg-accent no-hover:hover:bg-transparent"
       )}
       tabIndex={0}
       onKeyDown={handleKeyDown}
@@ -46,7 +46,7 @@ export const QuickQuiz = () => {
           e.stopPropagation()
           e.preventDefault()
         }}
-        className={`ml-2 flex space-x-2 ${!isActive && "w-11 opacity-0 group-hover:opacity-100"}`}
+        className={`ml-2 flex space-x-2 ${!isActive && "no-hover:group-hover:opacity-0 w-11 opacity-0 group-hover:opacity-100"}`}
       ></div>
     </div>
   )
